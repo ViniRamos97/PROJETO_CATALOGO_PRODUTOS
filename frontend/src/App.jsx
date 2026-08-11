fef739dcae50f080f877597fbb668ae90556515e
Aluno <e023aluno@outlook.com>
2026-08-11 19:59:34 -0300

    criação do formProduto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import FormProduto from "./components/FormProduto";
+
+function App(){
+
+  return(
+    <> 
+      <main className="container">
+
+    <FormProduto/>
+
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ListaProduto.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function ListaProduto(){
+
+    // return();
+}
+
+export default ListaProduto;
+
